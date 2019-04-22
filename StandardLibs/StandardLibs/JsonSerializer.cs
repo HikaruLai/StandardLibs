2edf618e73fdc501e4dee0386b384d6d44377bb0
HikaruLai <hikaru711228@gmail.com>
2019-04-23 00:11:14 +0800

    add JsonSerialser

---
+++ StandardLibs/StandardLibs/JsonSerializer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace StandardLibs.Utility
+{
+    public class JsonSerializer<T> : ISerializer<T>
+    {
+        public byte[] Serialize2Bytes(T entity)
+        {
+            return Encoding.UTF8.GetBytes(this.Serialize(entity));
+        }
+
+        public string Serialize(T entity)
+        {
+            return JsonConvert.SerializeObject(entity);
+        }
+
+        public T Deserialize(byte[] serialized)
+        {
+            return this.Deserialize(Encoding.UTF8.GetString(serialized));
+        }
+
+        public T Deserialize(string serialized)
+        {
+            return JsonConvert.DeserializeObject<T>(serialized);
+        }
+    }
+}
